4721b629caef5cd06abd25583927d7beff568c9b
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-28 14:16:43 +0300

    add

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,13 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        stack = [self.root]
+        while stack:
+            node = stack.pop()
+            if node and node.get('id') == id:
+                return node
+            if node and 'children' in node:
+                stack.extend(node['children'])
+        return None
